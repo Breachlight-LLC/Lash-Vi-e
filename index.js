144ea34431b5c4910fa76256a18a75ba4ad0db0d
Kevin Jones <kevin.d.jones.jr@gmail.com>
2024-01-29 19:33:28 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+var hoverCard1 = document.querySelector("div.card-1");
+
+hoverCard1.addEventListener('mouseover', () => {
+    hoverCard1.style.backgroundColor = 'teal';
+  });
+
+hoverCard1.addEventListener('mouseout', () => {
+    hoverCard1.style.backgroundColor = '';
+  });
+
+
+var hoverCard2 = document.querySelector("div.card-2");
+
+hoverCard2.addEventListener('mouseover', () => {
+    hoverCard2.style.backgroundColor = 'teal';
+});
+
+hoverCard2.addEventListener('mouseout', () => {
+    hoverCard2.style.backgroundColor = '';
+});
+
+    
+var hoverCard3 = document.querySelector("div.card-3");
+
+hoverCard3.addEventListener('mouseover', () => {
+    hoverCard3.style.backgroundColor = 'teal';
+ });
+
+hoverCard3.addEventListener('mouseout', () => {
+    hoverCard3.style.backgroundColor = '';
+    });
